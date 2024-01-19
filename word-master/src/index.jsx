889e90032bab97b9c59ab39f8e49097874d904db
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-19 18:22:03 +0900

    Add color theme & provider

--- word-master/src/index.jsx
+++ word-master/src/index.jsx
@@ -5,17 +5,38 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+// theme
+import { createTheme, ThemeProvider } from "@mui/material";
+
 // roboto 폰트
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+// theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffffff",
+      contrastText: "#404040",
+    },
+    secondary: {
+      light: "#E7E7E7",
+      main: "#B0B0B0",
+      dark: "#3E3E3E",
+      contrastText: "#ffffff",
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
