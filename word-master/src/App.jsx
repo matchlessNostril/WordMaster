ba3cff7f95b4ca6b012f538537b8f9838b092787
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-22 16:03:36 +0900

    Apply layout to all route

--- word-master/src/App.jsx
+++ word-master/src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Router from "./router/Router";
+import ColumnFlexBox from "./layout/ColumnFlexBox";
 
 function App() {
   return (
     <>
       <Header />
-      <Router />
+      <ColumnFlexBox>
+        <Router />
+      </ColumnFlexBox>
     </>
   );
 }
