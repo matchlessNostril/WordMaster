0090fdce7b26982e8f4cdd8811a1259ce0f1af66
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-26 19:11:33 +0900

    Add Header and TextField

--- word-master/src/pages/Voca/CreateVoca.jsx
+++ word-master/src/pages/Voca/CreateVoca.jsx
@@ -1,5 +1,25 @@
+// MUI
+import { Box, TextField, ListItem } from "@mui/material";
+// Component
+import Header from "../../components/Voca/CreateOrModifyVoca/Header";
+// Layout
+import ScrollList from "../../layout/ScrollList";
+
 const CreateVoca = () => {
-  return <></>;
+  return (
+    <Box sx={{ minWidth: "85vw", minHeight: "85vh" }}>
+      <Header isMdisabled={false} onClickHandler={() => {}} />
+      <ScrollList>
+        <ListItem>
+          <TextField
+            label="단어장 이름"
+            variant="standard"
+            sx={{ width: "100%" }}
+          />
+        </ListItem>
+      </ScrollList>
+    </Box>
+  );
 };
 
 export default CreateVoca;
