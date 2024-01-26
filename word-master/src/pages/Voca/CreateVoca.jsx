8c03b466e0e45ba99385a4bfa737c60bac279dba
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-26 21:30:53 +0900

    Add WordCard Component and Add Btn

--- word-master/src/pages/Voca/CreateVoca.jsx
+++ word-master/src/pages/Voca/CreateVoca.jsx
@@ -1,11 +1,15 @@
 // MUI
-import { Box, TextField, ListItem } from "@mui/material";
+import { Box, TextField, ListItem, IconButton } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 // Component
 import Header from "../../components/Voca/CreateOrModifyVoca/Header";
+import WordCard from "../../components/Voca/CreateOrModifyVoca/WordCard";
 // Layout
 import ScrollList from "../../layout/ScrollList";
 
 const CreateVoca = () => {
+  const numbers = Array.from({ length: 20 }, (_, index) => index + 1);
+
   return (
     <Box sx={{ minWidth: "85vw", minHeight: "85vh" }}>
       <Header disabled={false} onClickHandler={() => {}} />
@@ -16,6 +20,14 @@ const CreateVoca = () => {
             variant="outlined"
             sx={{ width: "100%" }}
           />
+        </ListItem>
+        {numbers.map((value) => (
+          <WordCard index={value} />
+        ))}
+        <ListItem sx={{ display: "flex", justifyContent: "center" }}>
+          <IconButton onClick={() => {}}>
+            <AddCircleIcon sx={{ fontSize: "40px" }} />
+          </IconButton>
         </ListItem>
       </ScrollList>
     </Box>
