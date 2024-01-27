831aa42695f110d6343938cb6e05aea94f5d259a
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-28 03:56:14 +0900

    Add Header

--- word-master/src/pages/Voca/Voca.jsx
+++ word-master/src/pages/Voca/Voca.jsx
@@ -1,7 +1,11 @@
 // Router
 import { useLocation, useNavigate } from "react-router-dom";
+// Hook
+import { useCallback } from "react";
 // MUI
-import { Box, Button } from "@mui/material";
+import { Box, Divider } from "@mui/material";
+// Component
+import SubHeader from "../../components/SubHeader";
 
 const Voca = () => {
   // location.state로 전달된 key, title, path 값 불러오기
@@ -13,24 +17,28 @@ const Voca = () => {
   // navigate
   const navigate = useNavigate();
 
+  // 수정 버튼 핸들러 함수
+  const onClickModifyBtn = useCallback(() => {
+    navigate("/SaveVoca", {
+      state: {
+        mode: "Modify",
+        path: path,
+        key: key,
+        title: title,
+      },
+    });
+  }, []);
+
   return (
     <>
-      <Box>
-        <Button
-          variant="outlined"
-          onClick={() =>
-            navigate("/SaveVoca", {
-              state: {
-                mode: "Modify",
-                path: path,
-                key: key,
-                title: title,
-              },
-            })
-          }
-        >
-          수정
-        </Button>
+      <Box sx={{ minWidth: "85vw", minHeight: "85vh" }}>
+        <SubHeader
+          title={title}
+          disabled={false}
+          btnName="수정"
+          onClickHandler={onClickModifyBtn}
+        />
+        <Divider sx={{ mt: 3, mb: 3 }} />
       </Box>
     </>
   );
