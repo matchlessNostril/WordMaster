d3cdbe8a6199ccdc6d88d527be1f9b0f2e2488a9
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-29 04:53:30 +0900

    Complete Voca Page and The Features

--- word-master/src/pages/Voca/Voca.jsx
+++ word-master/src/pages/Voca/Voca.jsx
@@ -1,7 +1,9 @@
 // Router
 import { useLocation, useNavigate } from "react-router-dom";
 // Hook
-import { useState, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
+// Custom Hook
+import useLoading from "../../hooks/useLoading";
 // MUI
 import {
   Box,
@@ -14,8 +16,13 @@ import {
 } from "@mui/material";
 // Component
 import SubHeader from "../../components/SubHeader";
+import Loading from "../../components/Loading";
+import WordCard from "../../components/Voca/Voca/WordCard";
 // Layout
 import RowSpaceBetween from "../../layout/RowSpaceBetween";
+import ScrollList from "../../layout/ScrollList";
+// API
+import { getList } from "../../service/database/getList";
 
 const Voca = () => {
   // location.state로 전달된 key, title, path 값 불러오기
@@ -23,6 +30,21 @@ const Voca = () => {
   const key = location.state.key;
   const title = location.state.title;
   const path = location.state.path;
+
+  // 단어 리스트 State
+  const [wordList, setWordList] = useState([{}]);
+
+  // 로딩 State와 Setter
+  const [onLoading, setOnLoading] = useLoading();
+
+  // 마운트 시, 단어 리스트 데이터 불러오기
+  useEffect(() => {
+    setOnLoading(true);
+    getList(`Voca/${path}/${title}`).then((list) => {
+      setWordList(list);
+      setOnLoading(false);
+    });
+  }, []);
 
   // navigate
   const navigate = useNavigate();
@@ -55,25 +77,50 @@ const Voca = () => {
           onClickHandler={onClickModifyBtn}
         />
         <Divider sx={{ mt: 3, mb: 3 }} />
-        <FormControl>
-          <RowSpaceBetween>
-            <RadioGroup
-              row
-              value={radio}
-              onChange={(event) => setRadio(event.target.value)}
-              sx={{ ml: 2 }}
-            >
-              <FormControlLabel value="word" control={<Radio />} label="단어" />
-              <FormControlLabel value="mean" control={<Radio />} label="뜻" />
-            </RadioGroup>
-            <FormControlLabel
-              control={
-                <Checkbox onChange={() => setChecked((prev) => !prev)} />
-              }
-              label="답 숨기기"
-            />
-          </RowSpaceBetween>
-        </FormControl>
+        {onLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <FormControl>
+              <RowSpaceBetween>
+                <RadioGroup
+                  row
+                  value={radio}
+                  onChange={(event) => setRadio(event.target.value)}
+                  sx={{ ml: 2 }}
+                >
+                  <FormControlLabel
+                    value="word"
+                    control={<Radio />}
+                    label="단어"
+                  />
+                  <FormControlLabel
+                    value="mean"
+                    control={<Radio />}
+                    label="뜻"
+                  />
+                </RadioGroup>
+                <FormControlLabel
+                  control={
+                    <Checkbox onChange={() => setChecked((prev) => !prev)} />
+                  }
+                  label="답 숨기기"
+                />
+              </RowSpaceBetween>
+            </FormControl>
+            <ScrollList maxHeight="65vh">
+              {wordList.map((word, index) => (
+                <WordCard
+                  key={index}
+                  index={index}
+                  word={word}
+                  sortingBy={radio}
+                  onHideAnswer={checked}
+                />
+              ))}
+            </ScrollList>
+          </>
+        )}
       </Box>
     </>
   );
