b7558bbc55697a691ac2d508982028ea2a39f3aa
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-29 17:36:44 +0900

    Add Header, TextField, Explanation UI

--- word-master/src/pages/Test/CreateTest.jsx
+++ word-master/src/pages/Test/CreateTest.jsx
@@ -1,5 +1,9 @@
+// Hook
 import { useState } from "react";
-import { Box, Button } from "@mui/material";
+// MUI
+import { Box, TextField, Divider, Typography, Button } from "@mui/material";
+// Component
+import SubHeader from "../../components/SubHeader";
 
 const Dir = ({ index }) => {
   const [showChild, setShowChild] = useState(false);
@@ -20,7 +24,31 @@ const ChildDir = ({ index }) => {
 const CreateTest = () => {
   return (
     <>
-      <Dir index={0} />
+      <Box sx={{ minWidth: "85vw", minHeight: "85vh" }}>
+        <SubHeader
+          title="테스트 만들기"
+          disabled={false}
+          btnName="만들기"
+          onClickHandler={() => {}}
+        />
+        <Box mt={2} p={1}>
+          <TextField
+            label="테스트 이름"
+            variant="outlined"
+            sx={{ width: "100%" }}
+          />
+        </Box>
+        <Divider sx={{ mt: 2, mb: 2 }} />
+        <Box sx={{ pl: 2, pr: 2 }}>
+          <Typography variant="h6" mb={1}>
+            <strong>단어장 선택</strong>
+          </Typography>
+          <Typography variant="body1" sx={{ maxWidth: "80vw" }}>
+            테스트 생성 시점 기준 단어 세트로 생성되며, 이후 단어 세트가
+            업데이트 되어도 테스트 내용은 바뀌지 않습니다.
+          </Typography>
+        </Box>
+      </Box>
     </>
   );
 };
