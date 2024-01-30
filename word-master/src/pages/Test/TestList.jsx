f35448bf6b12e9a5965eca9a7f0e6194d2aedd48
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-31 02:50:26 +0900

    Add Header UI and onClickHandler

--- word-master/src/pages/Test/TestList.jsx
+++ word-master/src/pages/Test/TestList.jsx
@@ -1,8 +1,38 @@
+// Router
+import { useNavigate } from "react-router-dom";
+// Context
+import { AuthContext } from "../../contexts/AuthContext";
+// Hook
+import { useContext } from "react";
+// MUI
+import { Box, Typography, IconButton } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 // Component
 import Transition from "../../components/Transition";
+// Layout
+import RowSpaceBetween from "../../layout/RowSpaceBetween";
 
 const TestList = () => {
-  return <>TestList데스</>;
+  // 사용자 정보
+  const { displayName } = useContext(AuthContext);
+
+  // navigate
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Box sx={{ minWidth: "85vw", minHeight: "85vh" }}>
+        <RowSpaceBetween>
+          <Typography variant="h5" ml={2}>
+            <strong>{displayName}</strong>님의 테스트
+          </Typography>
+          <IconButton onClick={() => navigate("/CreateTest")}>
+            <AddCircleIcon sx={{ fontSize: "40px" }} />
+          </IconButton>
+        </RowSpaceBetween>
+      </Box>
+    </>
+  );
 };
 
 export default Transition(TestList);
