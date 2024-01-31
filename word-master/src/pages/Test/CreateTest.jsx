b612a8ce79ed8b77b25f2ae19ef4b95ec6680dc9
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-31 21:21:29 +0900

    Fetch the saved wordList in wordTest and saved it at once in meanTest

--- word-master/src/pages/Test/CreateTest.jsx
+++ word-master/src/pages/Test/CreateTest.jsx
@@ -30,7 +30,11 @@ import Nofile from "../../components/NoFile";
 import ScrollList from "../../layout/ScrollList";
 // API
 import { getList } from "../../service/database/getList";
-import { setData, pushData } from "../../service/database/dataOperation";
+import {
+  getData,
+  setData,
+  pushData,
+} from "../../service/database/dataOperation";
 // Utils
 import listObjToArr from "../../utils/listObjToArr";
 
@@ -276,11 +280,17 @@ const CreateTest = () => {
       meanRound: 1,
     });
 
-    // 단어 리스트 저장
+    // wordTest, meanTest에 단어 리스트 저장
     for (let i = 0; i < wordList.length; i++) {
       await pushData(`Test/${testName}/wordList/wordTest/waiting`, wordList[i]);
-      await pushData(`Test/${testName}/wordList/meanTest/waiting`, wordList[i]);
     }
+    const waitingWordList = await getData(
+      `Test/${testName}/wordList/wordTest/waiting`
+    );
+    await setData(
+      `Test/${testName}/wordList/meanTest/waiting`,
+      waitingWordList
+    );
 
     // DB 저장 완료 후, 로딩 Off하고 화면 이동
     setOnLoading(false);
