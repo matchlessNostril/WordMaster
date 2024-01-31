b2435053bc911ce5e672e2beb3fb0108ffb60058
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-31 11:00:16 +0900

    Add Test Reset Feature

--- word-master/src/components/Test/SetTest/ProgressBar.jsx
+++ word-master/src/components/Test/SetTest/ProgressBar.jsx
@@ -1,11 +1,18 @@
+// Hook
+import { useState } from "react";
 // MUI
 import { Stack, Box, Typography, Slider, IconButton } from "@mui/material";
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
+// API
+import { getList } from "../../../service/database/getList";
+import { pushData, updateData } from "../../../service/database/dataOperation";
 
 const ProgressBar = ({ title, type, numOfPassed = 10, listLength }) => {
-  // Slider 정보
-  const percentage = Math.floor((numOfPassed / listLength) * 100);
-  const marks = [
+  // Slider 정보 State
+  const [percentage, setPercentage] = useState(
+    Math.floor((numOfPassed / listLength) * 100)
+  );
+  const [marks, setMarks] = useState([
     {
       value: percentage,
       label: numOfPassed.toString(),
@@ -14,7 +21,28 @@ const ProgressBar = ({ title, type, numOfPassed = 10, listLength }) => {
       value: 100,
       label: listLength.toString(),
     },
-  ];
+  ]);
+
+  // 리셋 함수
+  const onClickResetBtn = async () => {
+    // 통과된 단어 리스트 불러오기
+    const passedWordList = await getList(`Test/${title}/wordList/passed`);
+
+    // 대기 리스트로 이동
+    for (let i = 0; i < passedWordList.length; i++) {
+      await pushData(`Test/${title}/wordList/waiting`, passedWordList[i]);
+    }
+
+    // 통과된 단어 수 수정
+    if (type === "word") {
+      await updateData(`Test/${title}/info`, { numOfPassedWord: 0 });
+    } else {
+      await updateData(`Test/${title}/info`, { numOfPassedMean: 0 });
+    }
+
+    setPercentage(0);
+    setMarks((prev) => [{ value: 0, label: "0" }, prev[1]]);
+  };
 
   return (
     <Stack
@@ -32,13 +60,17 @@ const ProgressBar = ({ title, type, numOfPassed = 10, listLength }) => {
       </Box>
       <Box sx={{ flexGrow: 1 }}>
         <Slider
-          defaultValue={percentage}
+          value={percentage}
           marks={marks}
           disabled
           sx={{ "&.Mui-disabled": { color: "#535353" } }}
         />
       </Box>
-      <IconButton sx={{ ml: 2 }}>
+      <IconButton
+        onClick={onClickResetBtn}
+        disabled={!numOfPassed}
+        sx={{ ml: 2 }}
+      >
         <RestartAltIcon />
       </IconButton>
     </Stack>
