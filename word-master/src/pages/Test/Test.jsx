39f05fce7bd245abceea091a6026bbb932207009
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-31 21:42:32 +0900

    Fetch waiting word list

--- word-master/src/pages/Test/Test.jsx
+++ word-master/src/pages/Test/Test.jsx
@@ -1,4 +1,35 @@
+// Router
+import { useLocation } from "react-router-dom";
+// Hook
+import { useState, useEffect } from "react";
+// API
+import { getData } from "../../service/database/dataOperation";
+
 const Test = () => {
+  // location.state로 전달된 state 값 불러오기
+  const location = useLocation();
+  const { title, type, timer, numOfPassed, listLength } = location.state;
+
+  // wordList State
+  const [wordList, setWordList] = useState([]);
+
+  // 마운트 시 wordList 불러오기
+  useEffect(() => {
+    console.log("title :", title);
+    console.log("type :", type);
+    console.log("timer :", timer);
+    console.log("numOfPassed :", numOfPassed);
+    console.log("listLength :", listLength);
+
+    getData(`Test/${title}/wordList/${type}Test/waiting`).then(
+      (waitingWordList) => {
+        console.log(Object.entries(waitingWordList));
+        // 배열화 하여 저장
+        setWordList(Object.entries(waitingWordList));
+      }
+    );
+  }, []);
+
   return <>Test</>;
 };
 
