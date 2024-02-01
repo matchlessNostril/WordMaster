54e9eb8c026a2162f3d891ad578567ed4c4037c6
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-02-01 23:14:20 +0900

    Export Common Components at index.js

---
+++ word-master/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Transition } from "./Transition";
+export { default as Loading } from "./Loading";
+export { default as TextChip } from "./TextChip";
+export { default as ProgressBar } from "./ProgressBar";
