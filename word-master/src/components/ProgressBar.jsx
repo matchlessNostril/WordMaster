8d9462c57787a14571ac04b9438e8c36e47870da
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-02-01 23:16:19 +0900

    Create ProgressBar Component

---
+++ word-master/src/components/ProgressBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Slider } from "@mui/material";
+
+const ProgressBar = ({ percentage, numOfPassed, listLength }) => {
+  return (
+    <Box sx={{ flexGrow: 1 }} p={1}>
+      <Slider
+        value={percentage}
+        marks={[
+          {
+            value: percentage,
+            label: numOfPassed.toString(),
+          },
+          {
+            value: 100,
+            label: listLength.toString(),
+          },
+        ]}
+        disabled
+        sx={{
+          marginBottom: "-2.1vh",
+          "&.Mui-disabled": { color: "#535353" },
+          "& > .MuiSlider-thumb": {
+            width: "15px",
+            height: "15px",
+            backgroundColor: "white",
+            border: "2px solid #535353",
+          },
+          "& > .MuiSlider-markLabel": {
+            top: "-1.3vh",
+          },
+        }}
+      />
+    </Box>
+  );
+};
+
+export default React.memo(
+  ProgressBar,
+  (prevProps, nextProps) => prevProps.numOfPassed === nextProps.numOfPassed
+);
