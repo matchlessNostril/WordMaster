6fc90afaaf82c25d245172195226944f3abe8332
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-02-17 23:45:09 +0900

    Add Comment

--- word-master/src/pages/Start.jsx
+++ word-master/src/pages/Start.jsx
@@ -2,6 +2,7 @@ import { useIsPortrait } from "../hooks";
 import { Transition, Loading } from "../components";
 
 const Start = () => {
+  // 자동 로그인 사용자인지 확인
   const isLoginUser = localStorage.getItem("isLoginUser");
   const isPortrait = useIsPortrait();
 
@@ -11,6 +12,7 @@ const Start = () => {
         src={require("../assets/images/slogan.png")}
         style={{ width: isPortrait ? "70vw" : "30vw" }}
       />
+      {/* 자동 로그인 사용자의 경우, '로그인 중' 로딩 출력 */}
       {isLoginUser === "yes" && <Loading onMarginTop={false} />}
     </>
   );
