8c94daa3fb1c93d7845e7513c8f4d772c9b81d56
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-02-18 16:37:05 +0900

    Create Googlebtn Component
    Google Authentication Button

---
+++ word-master/src/pages/Join/components/GoogleBtn.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import StyledButton from "./StyledButton";
+
+const GoogleBtn = ({ method, onClickGoogleAuth }) => {
+  return (
+    <StyledButton variant="outlined" theme={theme} onClick={onClickGoogleAuth}>
+      <img
+        src={require("../../../assets/icons/google.png")}
+        alt="구글로 간편 시작"
+        style={{
+          width: "30px",
+          height: "30px",
+          marginRight: "20px",
+        }}
+      />
+      Google 계정으로 {method}
+    </StyledButton>
+  );
+};
+
+// method, onClickGoogleAuth는 각각 string, function으로 원시 타입이기 때문에,
+// 얕은 비교만 하면 되기 때문에 React.memo의 두 번째 매개변수를 비워도 됨.
+export default React.memo(GoogleBtn);
