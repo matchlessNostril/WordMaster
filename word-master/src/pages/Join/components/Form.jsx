46026f44461b3927c95dcb9bc1d6a4b95fe13875
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-02-18 16:59:31 +0900

    Add GoogleBtn

--- word-master/src/pages/Join/components/Form.jsx
+++ word-master/src/pages/Join/components/Form.jsx
@@ -1,9 +1,15 @@
 // Component
 import { Stack } from "@mui/material";
+import GoogleBtn from "./GoogleBtn";
+// API
+import { googleAuth } from "../../../service/auth";
 
 // prop이 method 밖에 없기 때문에 이름 props가 아닌 method로
 const Form = (method) => {
   return (
+    <Stack direction="column" spacing={3}>
+      <GoogleBtn {...method} onClickGoogleAuth={googleAuth} />
+    </Stack>
   );
 };
 
