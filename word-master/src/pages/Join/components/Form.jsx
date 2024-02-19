56a4c32b805c5ec949bff425320fcd5f56a82660
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-02-20 00:48:48 +0900

    Add Feature: Submit Button

--- word-master/src/pages/Join/components/Form.jsx
+++ word-master/src/pages/Join/components/Form.jsx
@@ -1,12 +1,13 @@
 // Hook
-import { useState, useEffect } from "react";
 // Component
+import { useState, useEffect, useMemo } from "react";
 import { Stack } from "@mui/material";
 import GoogleBtn from "./GoogleBtn";
 import TextDivider from "./TextDivider";
 import InputField from "./InputField";
+import SubmitBtn from "./SubmitBtn";
 // API
-import { googleAuth } from "../../../service/auth";
+import { googleAuth, Login, Register } from "../../../service/auth";
 // utils
 import {
   isValidNickname,
@@ -32,6 +33,19 @@ const initialJoinInfoValid = {
 const Form = ({ method }) => {
   const [joinInfo, setJoinInfo] = useState(initialJoinInfo);
   const [joinInfoValid, setJoinInfoValid] = useState(initialJoinInfoValid);
+
+  // joinInfoValid 값 바뀔 때만, 제출 버튼 활성화 여부 재계산
+  const disabled = useMemo(() => {
+    // 회원 가입 + nickname 유효성 통과되지 않은 경우 -> disabled를 true로
+    if (method === "회원 가입" && !joinInfoValid.nickname) return true;
+
+    // nickname은 유효성 통과된 상태이고,
+    // email, password까지 유효성 통과된 경우 -> disabled를 false로
+    if (joinInfoValid.email && joinInfoValid.password) return false;
+
+    // 하나라도 유효성이 통과되지 못한 경우 -> disabled를 true로
+    return true;
+  }, [joinInfoValid]);
 
   return (
     <Stack direction="column" spacing={3}>
@@ -59,6 +73,15 @@ const Form = ({ method }) => {
         validCheck={isValidPassword}
         showValue={showValue}
         onClickShowBtn={() => setShowValue((prev) => !prev)}
+      <SubmitBtn
+        method={method}
+        disabled={disabled}
+        onClickSubmitBtn={
+          method === "로그인"
+            ? () => Login(joinInfo.email, joinInfo.password)
+            : () =>
+                Register(joinInfo.nickname, joinInfo.email, joinInfo.password)
+        }
       />
     </Stack>
   );
